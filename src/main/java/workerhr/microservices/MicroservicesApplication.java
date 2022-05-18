9295d8cfe201e769616fa05e2f0af3bc01e7354d
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2022-05-18 20:32:34 -0300

    Alterando as configurações/incluindo dependencias para adicionar cliente no eureka server

--- src/main/java/workerhr/microservices/MicroservicesApplication.java
+++ src/main/java/workerhr/microservices/MicroservicesApplication.java
@@ -2,8 +2,10 @@ package workerhr.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroservicesApplication {
 
 	public static void main(String[] args) {
